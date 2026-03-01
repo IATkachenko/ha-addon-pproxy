ccbb3e96c507fba27227c06f25e3b21b123fd674
Igor <IATkachenko@gmail.com>
2026-03-02 01:44:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ CONFIG_PATH=/data/options.json
 scheme="$(bashio::config 'proxy_scheme')"
 host="$(bashio::config 'proxy_host')"
 port="$(bashio::config 'proxy_port')"
-user="$(bashio::config 'proxy_user')"
+user="$(bashio::config 'proxy_login')"
 password="$(bashio::config 'proxy_password')"
 
 proxy="${scheme}://${host}:${port}"
